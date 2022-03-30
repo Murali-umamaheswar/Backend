f34daeb285d3e6af1bef3a620e8d11bf412fe3ab
Kurella Uma Maheswar <umamaheswar.s@prodapt.com>
2022-03-30 14:35:54 +0530

    Backend

---
+++ src/test/java/com/SpringBoot/SpringBackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.SpringBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
